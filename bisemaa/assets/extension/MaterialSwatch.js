d2eb5eb73f9695b7995da02bf527e774500efcd0
Semegne <semegne.tafesse@bisema.com>
2026-02-20 09:37:22 -0500

    Feb20 at 937 AM

---
+++ bisemaa/assets/extension/MaterialSwatch.js
@@ -0,0 +1,93 @@
+class MaterialSwatchExtension extends Autodesk.Viewing.Extension {
+    constructor(viewer, options) {
+        super(viewer, options);
+        this._swatch = null;
+        this._urn = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6YmlzZW1hLWJ1Y2tldC9RLTAwMi0xMDAxX01hdC5pcHQ'; // Use your own swatch model URN
+    }
+
+    load() {
+        return true;
+    }
+
+    unload() {
+        return true;
+    }
+
+    /**
+     * Lists all available material presets.
+     * @async
+     * @returns {Map} Mapping of preset names to instances of THREE.Material.
+     */
+    async getPresets() {
+        if (!this._swatch) {
+            this._swatch = await this._loadSwatchModel(this._urn);
+        }
+        const presets = new Map();
+        const tree = this._swatch.getInstanceTree();
+        const frags = this._swatch.getFragmentList();
+        console.log(frags)
+        // console.log(tree.enumNodeChildren(tree.getRootId()));
+        tree.enumNodeChildren(tree.getRootId(), function (dbid) {
+            console.log(dbid)
+            if (tree.getChildCount(dbid) === 0) {
+                const name = tree.getNodeName(dbid);
+                console.log(tree)
+                console.log(name)
+                tree.enumNodeFragments(dbid, function (fragid) {
+                    if (!presets.has(name)) {
+                        presets.set(name, frags.getMaterial(fragid));
+                    }
+                }, true);
+            }
+        }, true);
+        return presets;
+    }
+
+    /**
+     * Applies material preset to specific object.
+     * @async
+     * @param {string} name Material preset name.
+     * @param {Autodesk.Viewing.Model} targetModel Model that contains the object to be modified.
+     * @param {number} targetObjectId DbID of the object to be modified.
+     */
+    async applyPreset(name, targetModel, targetObjectId) {
+        const presets = await this.getPresets();
+        if (!presets.has(name)) {
+            console.error('Material swatch not found', name);
+            return;
+        }
+        const material = presets.get(name);
+        const tree = targetModel.getInstanceTree();
+        const frags = targetModel.getFragmentList();
+        tree.enumNodeFragments(targetObjectId, function (fragid) {
+            frags.setMaterial(fragid, material);
+        }, true);
+        targetModel.unconsolidate();
+    }
+
+    async _loadSwatchModel(urn) {
+        const viewer = this.viewer;
+        return new Promise(function (resolve, reject) {
+            function onSuccess(doc) {
+                const viewable = doc.getRoot().getDefaultGeometry();
+                console.log(doc.getRoot());
+                viewer.addEventListener(Autodesk.Viewing.TEXTURES_LOADED_EVENT, function (ev) {
+                    if (ev.model._isSwatch) {
+                        resolve(ev.model);
+                    }
+                });
+                viewer.loadDocumentNode(doc, viewable, {
+                    preserveView: true,
+                    keepCurrentModels: true,
+                    loadAsHidden: true // <-- I see what you did there <_<
+                }).then(model => model._isSwatch = true);
+            }
+            function onError(code, msg) {
+                reject(msg);
+            };
+            Autodesk.Viewing.Document.load('urn:' + urn, onSuccess, onError);
+        });
+    }
+}
+
+Autodesk.Viewing.theExtensionManager.registerExtension('MaterialSwatchExtension', MaterialSwatchExtension);
